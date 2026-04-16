d30f8b1ad172b490e55cdb776b12f79f6a88cbaf
vnxuz <drobizkiarie@gmail.com>
2026-04-16 09:54:39 -0400

    Add form submission handler for registration

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
+    let name = document.getElementById("name").value;
+
+    document.getElementById("message").textContent =
+        "Thank you, " + name + "! You have registered.";
+});
